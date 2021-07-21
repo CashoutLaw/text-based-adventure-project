f1975860722fdd002663a68568b39ce2f41b4ea2
James <47540382+CashoutLaw@users.noreply.github.com>
2021-07-21 17:33:18 -0400

    Update main.js
    added function that checks for user input and takes the user input

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+var rooms = {
+  "room0":"you are in a dark place"
 
+
+}
+
+$(document).ready(function(){
+    $(document).keypress(function(key){
+        if(key.which == 13 && $('#UserInput').is(':focus')){
+          var value = $(#UserInput).val();
+        }
+    })
+  
+})
