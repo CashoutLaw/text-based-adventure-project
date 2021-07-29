fad96ec39c8101a6c354559f1b5ffd811d358b58
James <47540382+CashoutLaw@users.noreply.github.com>
2021-07-28 23:07:53 -0400

    Create rooms.js
    created the rooms where the story will be taking place

---
+++ rooms.js
@@ -0,0 +1,21 @@
+var rooms = {
+  "start":{
+    "description":"you are in a dark place", "directions":{
+    "north": "clearing1",
+      "west":"bridge"
+    }
+          }
+  "clearing1":{
+  "description":"something happened"
+}
+"stop1":{
+  "description":"another thing happened"
+}
+"bridge":{
+"description":"you arrive to a bridge"
+"directions":{
+  "east":"start",
+    "west":"bridge2"
+}
+}
+}
